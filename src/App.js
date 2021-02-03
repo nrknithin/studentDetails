4bea032b32aebc9d83d2240a1809b8d9e588f2e7
Nithin R Krishnan <nithinrkrishnan@dostomlabs.com>
2021-02-03 18:56:08 +0530

    Message

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import DataTable from "react-data-table-component";
+import editIcon from "./img/green-edit-icon.png";
+import deleteIcon from "./img/black-delete-icon.png";
 
 const App = () => {
   const customStyles = {
@@ -11,17 +13,53 @@ const App = () => {
     },
   };
 
-  const [data, setData] = useState([]);
-  const [firstName, setFirstName] = useState([]);
-  const [lastName, setLastName] = useState([]);
-  const [dob, setDOB] = useState([]);
-  const [contact, setContact] = useState([]);
+  const [studentData, setStudentData] = useState({
+    firstName: "",
+    lastName: "",
+    dob: "",
+    contactNo: "",
+  });
+  const [course, setCourse] = useState({
+    name: "",
+    details: "",
+  });
+  const [tableData, setTableData] = useState([]);
   const [firstNameError, setFirstNameError] = useState(false);
   const [lastNameError, setLastNameError] = useState(false);
   const [dobError, setDOBError] = useState(false);
   const [contactError, setContactError] = useState(false);
 
+  const EditStudent = ({ row }) => (
+    <img
+      onClick={() => editStudent(row)}
+      style={{ width: "20px", cursor: "pointer" }}
+      src={editIcon}
+    />
+  );
+  const editStudent = (row) => {};
+  const DeleteStudent = ({ row }) => (
+    <img
+      onClick={() => deleteStudent(row.id)}
+      style={{ width: "20px", cursor: "pointer" }}
+      src={deleteIcon}
+    />
+  );
+  const deleteStudent = (id) => {
+    setStudentData(
+      studentData.filter((item) => {
+        return item.id !== id;
+      })
+    );
+  };
   const [columns] = useState([
+    {
+      width: "35px",
+      cell: (row) => <EditStudent row={row} />,
+    },
+    {
+      width: "35px",
+      cell: (row) => <DeleteStudent row={row} />,
+    },
     {
       selector: "firstName",
       name: "First Name",
@@ -41,8 +79,22 @@ const App = () => {
       sortable: true,
     },
     {
-      selector: "contact",
+      selector: "contactNo",
       name: "Contact Number",
+      width: "150px",
+      sortable: true,
+    },
+  ]);
+  const [columnsCourse] = useState([
+    {
+      selector: "courseName",
+      name: "Course Name",
+      width: "150px",
+      sortable: true,
+    },
+    {
+      selector: "courseDetails",
+      name: "Course Details",
       width: "150px",
       sortable: true,
     },
@@ -50,66 +102,84 @@ const App = () => {
   const onFNameChange = (e) => {
     let firstNameFormat = /^[a-zA-Z]+$/;
     if (firstNameFormat.test(e.target.value)) {
-      setFirstName(e.target.value);
+      setStudentData({ ...studentData, firstName: e.target.value });
       setFirstNameError(false);
     } else setFirstNameError(true);
   };
   const onLNameChange = (e) => {
     let secondNameFormat = /^[a-zA-Z ]+$/;
     if (secondNameFormat.test(e.target.value)) {
-      setLastName(e.target.value);
+      setStudentData({ ...studentData, lastName: e.target.value });
       setLastNameError(false);
     } else setLastNameError(true);
   };
   const onDOBChange = (e) => {
-    setDOB(e.target.value);
+    setStudentData({ ...studentData, dob: e.target.value });
   };
   const onContactChange = (e) => {
     if (e.target.value < 9999999999) {
-      setContact(e.target.value);
+      console.log(studentData);
+      setStudentData({ ...studentData, contactNo: e.target.value });
       setContactError(false);
     } else setContactError(true);
   };
-  const handleAddNew = () => {
-    ChangeView(!gridView);
-    setFirstName("");
-    setLastName("");
-    setDOB("");
-    setContact("");
+  const onCourseNameChange = (e) => {
+    setCourse({ ...course, name: e.target.value });
   };
+  const onCourseDetailsChange = (e) => {
+    setCourse({ ...course, details: e.target.value });
+  };
+  const handleAddCourse = () => {};
+  // }
+  const handleAddStudent = () => {
+    if (
+      studentData.firstName !== "" &&
+      studentData.lastName !== "" &&
+      studentData.dob !== "" &&
+      studentData.contactNo !== ""
+    ) {
+      setTableData([...tableData, { id: Math.random() * 10, ...studentData }]);
 
-  const handleSubmit = () => {
-    if (firstName !== "" && lastName !== "" && dob !== "" && contact !== "") {
-      setData([
-        ...data,
-        {
-          firstName,
-          lastName,
-          dob,
-          contact,
-        },
-      ]);
-      ChangeView(!gridView);
+      ///Add Api for Student Data here student Data Available in studentData
     } else alert("all fields are required");
   };
 
-  const [gridView, ChangeView] = useState(true);
+  const [addCourse, ChangeCourseView] = useState(false);
   return (
     <div>
-      {gridView ? (
-        <div
-          style={{ position: "center !important", height: "70% !important" }}
-        >
-          <button onClick={handleAddNew}>Add New</button>
-          <DataTable
-            columns={columns}
-            data={data}
-            noHeader
-            defaultSortField="Rfqnum"
+      <button onClick={() => ChangeCourseView(!addCourse)}>
+        {addCourse ? "Add Student" : "Add Course"}
+      </button>
+      {addCourse ? (
+        <div className="container">
+          <div>
+            <label for="courseName">Course Name</label>
+            <input
+              type="text"
+              id="courseName"
+              name="courseName"
+              onChange={onCourseNameChange}
+              value={course.name}
+            />
+          </div>
+          <div>
+            <label for="courseDetails">Course Details</label>
+            <input
+              type="text"
+              id="courseDetails"
+              name="courseDetails"
+              onChange={onCourseDetailsChange}
+              value={course.details}
+            />
+          </div>
+          <div>
+            <button onClick={handleAddCourse}>Add Course</button>
+          </div>
+          <Table
+            columns={columnsCourse}
+            data={course}
             customStyles={customStyles}
-            pagination
-            fixedHeader
-            fixedHeaderScrollHeight="400px"
+            exp={false}
           />
         </div>
       ) : (
@@ -121,9 +191,8 @@ const App = () => {
               id="firstName"
               name="firstName"
               onChange={onFNameChange}
-              // value={firstName}
             />
-            <label hidden={!firstNameError}>Invalid Data</label>
+            <label hidden={!firstNameError}>Invalid studentData</label>
           </div>
           <div>
             <label for="lastName">Last Name</label>
@@ -132,9 +201,8 @@ const App = () => {
               id="lastName"
               name="lastName"
               onChange={onLNameChange}
-              // value={lastName}
             />
-            <label hidden={!lastNameError}>Invalid Data</label>
+            <label hidden={!lastNameError}>Invalid studentData</label>
           </div>
           <div>
             <label for="dob">DOB</label>
@@ -142,27 +210,58 @@ const App = () => {
             <label hidden={!dobError}>Not a Proper Date</label>
           </div>
           <div>
-            <label for="contact">Contact Number</label>
+            <label for="contactNo">Contact Number</label>
             <input
               min="1111111111"
               max="9999999999"
               type="number"
-              id="contact"
-              name="contact"
+              id="contactNo"
+              name="contactNo"
               onChange={onContactChange}
-              value={contact}
+              value={studentData.contactNo}
             />
             <label hidden={!contactError}>Not a proper Contact Number</label>
           </div>
           <br />
           <div>
-            <button onClick={() => ChangeView(!gridView)}>Cancel</button>
-            <button onClick={handleSubmit}>Submit</button>
+            <button onClick={handleAddStudent}>Add Student</button>
+          </div>
+          <div
+            style={{ position: "center !important", height: "70% !important" }}
+          >
+            {/* <Table
+              columns={columns}
+              studentData={studentData}
+              customStyles={customStyles}
+              columnsCourse={columnsCourse}
+              exp={true}
+            /> */}
           </div>
         </div>
       )}
+      <Table
+        columns={columns}
+        data={tableData}
+        customStyles={customStyles}
+        exp={false}
+      />
     </div>
   );
 };
 
+const Table = (props) => {
+  console.log(props.data);
+  return (
+    <DataTable
+      columns={props.columns}
+      data={props.data}
+      noHeader
+      customStyles={props.customStyles}
+      pagination
+      fixedHeader
+      fixedHeaderScrollHeight="400px"
+      expandOnRowClicked
+    />
+  );
+};
 export default App;
